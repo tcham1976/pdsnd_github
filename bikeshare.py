7800874c6b9e4ce44136db820f453ad6705e02f4
root <root@localhost>
2026-07-28 05:12:04 +0000

    Refactor bikeshare code for readability

--- bikeshare.py
+++ bikeshare.py
@@ -189,7 +189,7 @@ def trip_duration_stats(df):
 
 
 def user_stats(df):
-    """Displays statistics on bikeshare users."""
+    """Displays statistics on bikeshare users, including user type and gender"""
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
